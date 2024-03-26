a415e833545c18b698c9dbaae44fe50a4f36d11d
Adrian De Guzman <adrian.deguzman@student.ateneo.edu>
2024-03-26 16:37:37 +0800

    Updated urls.py

--- taskapp/tasks/urls.py
+++ taskapp/tasks/urls.py
@@ -4,10 +4,10 @@ from .views import task_list, task_detail, index, TaskListView,TaskDetailView
 
 urlpatterns = [
     # path('', task_list, name="task_list"),
-    path('', TaskListView.as_view(), name="task_list"),
+    path('list', TaskListView.as_view(), name="task_list"),
     # path('<int:pk>/detail', task_detail, name="task_detail"),
     path('<int:pk>/detail', TaskDetailView.as_view(), name="task_detail"),
-    path('index', index, name='index')
+    path('', index, name='index')
 ]
 
 app_name = 'tasks'
